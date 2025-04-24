4b1cc610d60e364e3a6a963876a5af5a1c58a923
insubkim <j.j.kant1948@gmail.com>
2025-04-24 09:51:11 -0400

    add jiffies

--- ex07/debugfs.c
+++ ex07/debugfs.c
@@ -5,6 +5,7 @@
 #include <linux/kernel.h>	/* Needed for KERN_INFO */
 #include <asm/uaccess.h>
 #include <linux/debugfs.h>
+#include <linux/jiffies.h>
 
 //https://www.kernel.org/doc/html/v6.15-rc3/filesystems/debugfs.html
 
@@ -21,11 +22,54 @@ static ssize_t my_read (struct file *file, char __user *user, size_t size, loff_
         char *s = "inskim";
         size_t err = 0;
 
-        size = min_t(size_t, size, 6);
+        size = min_t(size_t, size, 7);
         err = copy_to_user(user, s, size);
 
         if (err != 0)
                 printk(KERN_INFO "my_read - copy_to_user return err[%zu] !\n", err);
+
+
+        return size;
+}
+
+static ssize_t jiffies_read (struct file *file, char __user *user, size_t size, loff_t *loff)
+{
+        printk(KERN_INFO "mjiffies_read CALLED !\n");
+
+        char s[256] = {0,};
+        size_t err = 0;
+	
+	u64 jiffies = get_jiffies_64();
+        
+        printk(KERN_INFO "jiffies CALLED %llu!\n", jiffies);
+	
+	int len = 0;
+	int i = 0;
+	
+	while (jiffies > 0)
+	{
+		s[i++] = jiffies % 10 + '0';
+		jiffies /= 10;
+	}
+
+	len = i;
+	
+	i = 0;
+	int j = len - 1;
+	while (j > i)
+	{
+		char c = s[j];
+		s[j] = s[i];
+		s[i] = c;
+		i++; j--;
+	}
+	
+
+	size = min_t(size_t, size, len);
+        err = copy_to_user(user, s, size);
+
+        if (err != 0)
+                printk(KERN_INFO "jiffies - copy_to_user return err[%zu] !\n", err);
 
 
         return size;
@@ -51,6 +95,14 @@ static struct file_operations fops = {
     .write = my_write
 };
 
+static struct file_operations fops_jiffies = {
+    .owner = THIS_MODULE,
+    .open = my_open,
+    .release = my_release,
+    .read = jiffies_read,
+    .write = my_write
+};
+
 struct dentry* g_log_dir;
 
 static int debugfs_init(void)
@@ -69,8 +121,8 @@ static int debugfs_init(void)
 	int value = 42;
 
 	struct dentry *id = debugfs_create_file("id", 0777, log_dir, (void*)&value, &fops);
-	struct dentry *jiffies = debugfs_create_file("jiffies", 0444, log_dir, (void*)&value, &fops);
-	struct dentry *foo = debugfs_create_file("foo", 0444, log_dir, (void*)&value, &fops);
+	struct dentry *jiffies = debugfs_create_file("jiffies", 0444, log_dir, (void*)&value, &fops_jiffies);
+	struct dentry *foo = debugfs_create_file("foo", 0744, log_dir, (void*)&value, &fops);
 
 	if ((id == NULL) || (jiffies == NULL) || (foo == NULL))
 	{
